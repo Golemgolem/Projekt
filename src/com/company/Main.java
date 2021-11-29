2e5f66318b4894a4864a503f0104df7b6da7638d
honza <honzanespurek@seznam.cz>
2021-11-29 16:19:55 +0100

    Test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,6 @@
 package com.company;
+import com.company.Class1;
+
 import java.util.Scanner;
 import java.util.Arrays;
 public class Main {
@@ -6,5 +8,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("bagr");
         System.out.println(Class1.cislo(5));
+        System.out.println("7");
+        System.out.println("5");
     }
 }
