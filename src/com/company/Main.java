d565bc9bbf75bc05e1ff57abf1a29640284c1d55
honza <honzanespurek@seznam.cz>
2021-11-29 16:05:53 +0100

    bagr

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("bagr");
+    }
+}
